c411fc3ba69922f7a3af8887e17016e92e6acc80
namansnghl <48488518+namansnghl@users.noreply.github.com>
2025-03-27 19:00:35 -0400

    added routes for frontend

--- backend.py
+++ backend.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 husky_app = Flask(__name__)
 
 
+# Route for homepage
 @husky_app.route("/")
 def index():
-    return "This is home page"
+    return send_from_directory("frontend", 'index.html')
+
+
+# Catch-all route for all other files
+@husky_app.route("/<path:filename>")
+def frontend_files(filename):
+    return send_from_directory("frontend", filename)
 
 
 if __name__ == "__main__":
-    husky_app.run()
+    husky_app.run(debug=True)
