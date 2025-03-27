4d0a4ef919f58489532d72186989cf8f6fa146b6
namansnghl <48488518+namansnghl@users.noreply.github.com>
2025-03-27 17:47:31 -0400

    rearrange argparse before imports

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
+# Do not change, intentionally running argparse before imports
 import argparse
-import utils  # This loads vars, do not remove
+parser = argparse.ArgumentParser(description="RAG Chatbot System")
 
+# Create a mutually exclusive group for the two main options
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("--pipeline", action="store_true",
+                   help="Rerun the data pipeline (scrapping and preprocessing)")
+group.add_argument("--chatbot", action="store_true",
+                   help="Start the RAG chatbot on CLI")
+group.add_argument("--server", action="store_true",
+                   help="Start the RAG flask server")
+
+# Parse arguments
+args = parser.parse_args()
+
+
+import utils  # This loads vars, do not remove
 import scrapper
 import preprocessing
 import model
+from backend import husky_app
 
 
 def run_data_pipeline():
@@ -12,19 +28,6 @@ def run_data_pipeline():
 
 
 if __name__ == "__main__":
-    # Set up argument parsing
-    parser = argparse.ArgumentParser(description="RAG Chatbot System")
-
-    # Create a mutually exclusive group for the two main options
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--pipeline", action="store_true",
-                       help="Rerun the data pipeline (scrapping and preprocessing)")
-    group.add_argument("--chatbot", action="store_true",
-                       help="Start the RAG chatbot")
-
-    # Parse arguments
-    args = parser.parse_args()
-
     # Execute the selected option
     if args.pipeline:
         print("Running data pipeline...")
@@ -33,3 +36,6 @@ if __name__ == "__main__":
     elif args.chatbot:
         print("Starting RAG chatbot...")
         model.run_rag_claude()
+    elif args.server:
+        print("Starting flask server...")
+        husky_app.run()
